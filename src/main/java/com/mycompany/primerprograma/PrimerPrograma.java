43330cede0a0fc1bb023ff77c6660beb1ca58cfd
axelsmith11 <amestanza@pucp.edu.pe>
2025-01-19 12:22:37 -0500

    primerCambioCalculadora

---
+++ src/main/java/com/mycompany/primerprograma/PrimerPrograma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerprograma;
+
+/**
+ *
+ * @author Administrador
+ */
+public class PrimerPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
